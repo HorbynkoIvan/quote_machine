1cf54c6abb57d60b8ba081559281b5c1c38e8d81
Ivan <horbynko.i@brights.cloud>
2018-12-19 18:49:19 +0200

    added actions, provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 import App from './App';
-import 'font-awesome/css/font-awesome.min.css'
+import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import reducers from './reducers'
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    ,
+    document.getElementById('root'));
 
 
---
+++ src/reducers/quotes.js
@@ -0,0 +1,10 @@
+import {FETCH_QOUTES_START, FETCH_PHONES_SUCCESS, FETCH_PHONES_FAILURE} from "../actionTypes";
+
+const initialStates = {};
+
+export default (state = initialStates, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
