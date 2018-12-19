c6d876880ac9ab14d1627b0061224ca48a316f79
Ivan <horbynko.i@brights.cloud>
2018-12-19 15:54:40 +0200

    created enviroment

---
+++ build/precache-manifest.b5fa77b4b60f36a3929361fd306762c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5101dacb62cbb3c1d51",
+    "url": "/static/js/main.d5101dac.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "0a0e1f8abc4bcaa9cac9278ff70b9f31",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/common.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React svsoinvsnvpsdnvmposmvsmv
-          </a>
-        </header>
-      </div>
+        <div id="wrapper">
+            <div id="quote-box">
+                <div className="quote-text">
+                    <i className="fa fa-quote-left"> </i><span id="text"></span>
+                </div>
+                <div className="quote-author">
+                    - <span id="author"></span>
+                </div>
+                <div className="buttons">
+                    <a className="button" id="tweet-quote" title="Tweet this quote!" target="_blank">
+                        <i className="fa fa-twitter"></i>
+                    </a>
+                    <a className="button" id="tumblr-quote" title="Post this quote on tumblr!" target="_blank">
+                        <i className="fa fa-tumblr"></i>
+                    </a>
+                    <button className="button" id="new-quote">New quote</button>
+                </div>
+            </div>
+            <div className="footer"> by <a href="https://github.com/HorbynkoIvan/quote_machine_react">DidIvan</a></div>
+        </div>
     );
   }
 }
