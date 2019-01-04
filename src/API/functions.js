95b7b88fcb52531e019ea09a6cec167a8de9237f
Horbynko Ivan <horbynko.i@gmail.com>
2019-01-04 22:29:54 +0200

    added array of colors

--- src/API/functions.js
+++ src/API/functions.js
@@ -8,13 +8,8 @@ export const getRandomQuote = (obj) => {
     return quote;
 };
 
-export const getNextQuote = (quote) =>{
-
-}
-/*
-let quotesLenght = qoutes.length;
 const colors = [
-    'red', 'green', 'blue', 'white', 'purple', 'yellow', 'pink', 'silver', 'gray', 'orange'
+    '#e57373', '#f06292', '#b71c1c', '#880e4f', '#9c27b0', '#673ab7', '#4a148c', '#311b92', '#009688', '#4caf50',
+    '#00bfa5', '#00c853', '#64dd17', '#aeea00', '#5d4037', '#616161', '#607d8b', '#ff9100', '#ff3d00', '#ff6f00'
 ];
 
-let randomIndex = Math.floor(Math.random() * quotesLenght);*/
